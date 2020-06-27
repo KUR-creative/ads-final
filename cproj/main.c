6c0a50216cb06988675ba36ed9a85f4e269dba31
kur <rhdnfka94@gmail.com>
2020-06-27 19:29:46 +0900

    Change project structure, Add data

---
+++ cproj/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int y;
+    int x;
+    int i;
+} YXI;
+
+YXI arr[1000000]; 
+YXI* x_sorted[1000000]; // x sorted
+YXI* y_sorted[1000000]; // y sorted
+
+int main(void){
+    printf("start\n");
+    printf("[%d]", sizeof(int));
+    printf("[%d]", sizeof(arr));
+    printf("[%d]", sizeof(x_sorted));
+    printf("[%d]", sizeof(y_sorted));
+
+    //YXI* arr = malloc(sizeof(YXI) * 1000000);
+
+    printf("end\n");
+    return 0;
+}
---
+++ plot.py
@@ -0,0 +1,2 @@
+with open('./pin_1.txt') as inp:
+    print(inp.readlines()[:3])
